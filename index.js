da542f33e7ec8cae3521c9d83fef8a641b3dcab9
pedrooov <23pedrobarbosa@gmail.com>
2024-01-04 19:18:50 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,26 @@
+calcularNivel(86, 26)
+
+function calcularNivel(vitorias, derrotas) {
+	//calcula o saldo de rankeadas
+    let saldoVitorias = vitorias - derrotas
+    //determina o nível com base na quantidade de vitórias
+    let nivel
+    //estrutura de decisão
+    if(vitorias <= 10){
+		nivel = "Ferro"
+	}else if (vitorias >= 11 && vitorias <= 20){
+		nivel = "Bronze"
+	}else if (vitorias >=  21 && vitorias <= 50){
+		nivel = "Prata"
+	}else if (vitorias >= 51 && vitorias <= 80){
+		nivel = "Ouro"
+	}else if (vitorias >= 81 && vitorias <= 90){
+		nivel = "Diamante"
+	}else if (vitorias >= 91 && vitorias <= 100){
+		nivel = "Lendario"
+	}else{
+		nivel = "Imortal"
+	}
+    //mensagem final
+    console.log(`O herói tem um saldo de ${saldoVitorias} e está no nível ${nivel}`)
+}
